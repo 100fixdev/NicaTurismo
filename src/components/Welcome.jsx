6780de42fd37d3fdf6c15a1a2835a99a1b6cba8b
Felix2311 <f.ramirezl@fia.unicit.edu.ni>
2025-09-28 19:19:02 -0600

    feature: se agrego una mejor estilado para zona de imagenes y se agrego mejores colores

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,15 +5,21 @@ import {useState} from "react";
 export default function Welcome() {
 
   const [activeIndex, setActiveIndex] = useState(null);
-  const items = [1, 2, 3, 4, 5];
+  const items = [
+    { id: 1, img: "src/images/hoteles/habitacion-1-web.webp", title: "Hospedaje" },
+    { id: 2, img: "src/images/hoteles/comedor-web.webp", title: "Restaurantes" },
+    { id: 3, img: "src/images/alqulerdeauto2.png", title: "Transportes" },
+    { id: 4, img: "src/images/sandb-boarding.jpg", title: "Experiencias" },
+    { id: 5, img: "src/images/fondo-rural.jpg", title: "Comunidad" },
+  ];
 
   return (
     <div className="welcome__container">
       <section className="section__hero">
-        <h1>Welcome to NicaTurismo</h1>
-        <div className="container__hero">
+        <h1>Welcome to Nicaragua</h1>
+        {/*<div className="container__hero">
           <img src="" alt="lugares de Nicaragua" />
-        </div>
+        </div>*/}
       </section>
 
       <div className="section__description">
@@ -64,26 +70,27 @@ export default function Welcome() {
         <div className="section__Ofrecemos--title">
           <h2>¿Qué nos diferencia de otras plataformas?</h2>
           <p>
-            En NicaTurismo, nos dedicamos a ofrecerte las mejores experiencias
-            turísticas en Nicaragua. Desde tours personalizados hasta
-            alojamiento de calidad, estamos aquí para hacer de tu viaje una
-            experiencia inolvidable.
+            En NicasTour, nos dedicamos a ofrecerte las mejores experiencias
+            turísticas en Nicaragua. Desde alojamiento de calidad, hasta pago de entradas turisticas
+            estamos aquí para hacer de tu viaje una
+            experiencia inolvidable.  
           </p>
         </div>
 
         <div className="container__Ofrecemos--images">
-          {items.map((item, index) => (
-            <div
-              key={index}
-              className={`Ofrecemos__images--item ${
-                activeIndex === index ? "active" : ""
-              }`}
-              onMouseEnter={() => setActiveIndex(index)} // cuando paso el mouse, activo ese div
-            >
-              {item}
+        {items.map((item, index) => (
+          <div
+            key={item.id}
+            className={`Ofrecemos__images--item ${activeIndex === index ? "active" : ""}`}
+            onMouseEnter={() => setActiveIndex(index)}
+            style={{ backgroundImage: `url(${item.img})` }}
+          >
+            <div className="Ofrecemos__overlay">
+              <span>{item.title}</span>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
       </section>
 
       <section className="section__video">
